de4b244dccba3f44726d87e7e9dcbce096f53997
Axel David Plascencia Silva <pablitobanana1@gmail.com>
2022-04-27 18:54:45 -0500

    Agregar Cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="HomeView">
     <ToolBar />
+    <PokeCards />
   </div>
 </template>
 
 <script>
 import ToolBar from '@/components/ToolBar.vue';
+import PokeCards from '@/components/PokeCards.vue';
 export default {
   name: 'HomeView',
   components: {
     ToolBar,
+    PokeCards,
   }
 }
 </script>
