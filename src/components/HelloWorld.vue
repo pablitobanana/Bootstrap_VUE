f225ffa3f034fed94bc4387c917d0c5219c8a6f8
Axel David Plascencia Silva <pablitobanana1@gmail.com>
2022-04-27 17:24:11 -0500

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="helloworld">
+    <h1>hola</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+    <helloworld />
+</template>
+
+<script>
+  import helloworld from '../components/HelloWorld.vue';
+export default {
+  name: 'HomeView',
+  components: {
+    helloworld,
+  }
+}
+</script>
